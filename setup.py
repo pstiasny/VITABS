bc98f7b1033aa30f1eefb56ed358c82647ee8ea9
Paweł Stiasny <pawelstiasny@gmail.com>
2011-07-27 03:04:06 +0200

    Arrange files for a distutils package, setup meta

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name='VITABS',
+	version='0.1dev',
+	author='Pawel Stiasny',
+	packages=['vitabs'],
+	scripts=['bin/vitabs'],
+	url='http://github.com/PawelStiasny/VITABS',
+	license='COPYING',
+	description='Vi-inspired guitar tab editor'
+	long_descripton=open('README').read(),
+)
