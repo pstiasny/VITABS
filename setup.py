2e3fd7f9a3e4e8195cfb23a7cacf2bfb7ce59337
Paweł Stiasny <pawelstiasny@gmail.com>
2011-08-10 18:52:57 +0200

    song structure player

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	version='0.1dev',
 	author='Pawel Stiasny',
 	packages=['vitabs'],
-	scripts=['bin/vitabs'],
+	scripts=['bin/vitabs', 'bin/vitabs-song'],
 	url='http://github.com/PawelStiasny/VITABS',
 	license='COPYING',
 	description='Vi-inspired guitar tab editor',
