024732172276cc1dd42163688a967eeefa1e527c
Paweł Stiasny <pawelstiasny@gmail.com>
2017-05-27 12:32:26 +0200

    update metadata for a PyPI release

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='VITABS',
-    version='0.1dev',
+    name='vitabs',
+    version='0.1',
     author='Pawel Stiasny',
+    author_email='pawelstiasny@gmail.com',
     packages=['vitabs'],
     scripts=['bin/vitabs'],
-    url='http://github.com/PawelStiasny/VITABS',
-    license='COPYING',
+    url='http://github.com/pstiasny/VITABS',
+    license='GNU General Public License',
     description='Vi-inspired guitar tab editor',
     long_description=open('README').read(),
 )
